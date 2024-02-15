6d869ea741af92ae1375b28fbd53022665e52cb3
unknown <SunilLimje1979>
2024-02-15 12:07:03 +0530

    new

--- appointment/urls.py
+++ appointment/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('appointment/admin/', admin.site.urls),
     path("appointment/api/", include("appointmentapp.urls")),
